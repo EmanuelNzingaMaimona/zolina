a44372c8e093bf8976a70ac762e94020481cd609
Emanuel Nzinga Maimona <emanuelmaimona@gmail.com>
2022-09-01 10:17:30 +0200

    u

--- hemoangola/settings.py
+++ hemoangola/settings.py
@@ -137,9 +137,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'mymedia')
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'microluzangola@gmail.com'
-EMAIL_HOST_PASSWORD = 'devmicroluz@2021'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = '587'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = 'microluzangola@gmail.com'
+# EMAIL_HOST_PASSWORD = 'devmicroluz@2021'
